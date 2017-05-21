51612119b8c2939df61e853df8cde1c3553c16a1
Matteo Bogo <matteo.bogo@gmail.com>
2017-05-21 16:07:39 +0200

    now collector receive double instead of integer

--- erbal-collector/src/main/java/com.erbal/domain/NodeData.java
+++ erbal-collector/src/main/java/com.erbal/domain/NodeData.java
@@ -24,7 +24,7 @@ public class NodeData {
   private String sectorId;
 
   @Field(SensorsDataParams.NODE_VOLTAGE)
-  private int voltage;
+  private double voltage;
 
   @Field(SensorsDataParams.NODE_SAMPLE)
   private SampleData sample;
